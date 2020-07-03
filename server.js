078196915eb5a54c8d4412fc3011c137f31ea7ad
GideonBrimleaf <chrisdryden1@gmail.com>
2020-07-03 15:55:18 +0100

    can successfully write to file

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const fs = require('fs')
 const characters = require('./characters.json')
+const bodyParser = require('body-parser')
+const fs = require('fs')
 
 app.use(cors())
+app.use(bodyParser.json())
 
 app.get('/', function(req, res){
-  res.send(characters)
+  res.send("I'm Mary Poppins Y'All!")
+})
+
+app.get('/characters', function(req, res){
+  res.json(characters)
+})
+
+app.post('/characters', function(req, res){
+  const updated_characters = JSON.stringify({"characters": req.body})
+  fs.writeFile('./characters.json', updated_characters, (err) => {
+    if (err) console.log(err)
+    console.log('Successfully wrote to file')
+  })
 })
 
 app.listen(3000, function(){
